9e4d7b71104f38f59891eae0cff6323400336af2
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-14 19:38:11 +0600

    Update Banner.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      
+      <Banner />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,12 +4,10 @@ import { Box,Image } from '@chakra-ui/layout'
 const Banner = () => {
     return (
         <div>
-            <Header>
-                <Box d='flex' alignItems='center' justifyContent='space-between' ml={6} mr={6}>
-                    <Box></Box>
-                    <Box></Box>
-                </Box>
-            </Header>
+            <Box d='flex' alignItems='center' justifyContent='space-between' ml={6} mr={6}>
+                <Box></Box>
+                <Box></Box>
+            </Box>
         </div>
     )
 }
