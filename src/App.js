327f477d1e1739e22d4bee097a0db76a0f6bf3b8
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-14 20:10:23 +0600

    Add Main.js file to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Banner from './components/Banner';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Main />
     </div>
   );
 }
