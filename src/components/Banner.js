9905d6abba51e18bacdd2839ccda93fe8ecc818f
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-14 19:59:11 +0600

    Update Banner.js file

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Box,Image } from '@chakra-ui/layout'
+import { Box } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/image'
+import brandLogo from './logo.jpg'
+import { Button } from '@chakra-ui/button'
 
 const Banner = () => {
     return (
         <div>
             <Box d='flex' alignItems='center' justifyContent='space-between' ml={6} mr={6}>
-                <Box></Box>
-                <Box></Box>
+                <Image boxSize='90px' src={brandLogo} alt='brand'/>
+                <Box>
+                    <Button>Log In</Button>
+                </Box>
             </Box>
         </div>
     )
