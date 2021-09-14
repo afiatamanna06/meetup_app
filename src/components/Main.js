5895ff595b514f59610a5991ab02593c4d45c842
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-14 23:46:06 +0600

    Update Main.js file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Box } from '@chakra-ui/layout'
+import { Container } from '@chakra-ui/layout'
 
 const Main = () => {
     return (
         <div>
-            
+            <Box>
+                <Container>
+                    <Box>
+                        
+                    </Box>
+                </Container>
+            </Box> 
         </div>
     )
 }
