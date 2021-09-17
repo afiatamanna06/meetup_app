3852be5683b4c0cfcd1e6c2a9a76837f539a621e
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-17 20:04:31 +0600

    Update ItWorks.js file

--- src/components/ItWorks.js
+++ src/components/ItWorks.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Box, Container, Heading } from '@chakra-ui/layout'
 
 const ItWorks = () => {
     return (
         <div>
-            
+            <Box>
+                <Container>
+                    <Heading></Heading>
+                </Container>
+            </Box>
         </div>
     )
 }
