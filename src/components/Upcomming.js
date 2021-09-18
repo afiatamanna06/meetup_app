4bd248f3b83d54ff24574a05c08557859ce919a6
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-18 21:24:36 +0600

    Update Upcomming.js file

--- src/components/Upcomming.js
+++ src/components/Upcomming.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Box } from '@chakra-ui/layout'
+import Upcommingone from '../assets/upcomming-one.png'
+import Upcommingtwo from '../assets/upcomming-two.jpg'
+import Upcommingthree from '../assets/upcomming-three.jpg'
+import Upcommingfour from '../assets/upcomming-four.jpg'
 
 const Upcomming = () => {
     return (
