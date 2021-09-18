3175f761708dd508058acf6b5a2e36b73350888d
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-18 23:21:18 +0600

    Update Upcomming.js file

--- src/components/Upcomming.js
+++ src/components/Upcomming.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Container } from '@chakra-ui/layout'
+import { Box, Container, Heading } from '@chakra-ui/layout'
 import Upcommingone from '../assets/upcomming-one.png'
 import Upcommingtwo from '../assets/upcomming-two.jpg'
 import Upcommingthree from '../assets/upcomming-three.jpg'
@@ -11,7 +11,11 @@ const Upcomming = () => {
             <Box mt='20'>
                 <Container maxWidth='container.xl'>
                     <Box display='flex' alignItems='center' justifyContent='space-between' mb='7'>
-                        <Box></Box>
+                        <Box>
+                            <Heading as='h3' alignItems='center' size='lg'>
+                                Upcoming online events
+                            </Heading>
+                        </Box>
                         <Box></Box>
                     </Box>
                 </Container>
