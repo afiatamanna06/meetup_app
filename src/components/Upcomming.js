397f2f31aa61f779a5b647f21cae546692c17691
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-19 20:23:48 +0600

    Update Upcomming.js file

--- src/components/Upcomming.js
+++ src/components/Upcomming.js
@@ -175,9 +175,22 @@ const Upcomming = () => {
                         {/* 4th column */}
                         <Box width='100%' align='left'>
                             <Box position='relative'>
-                            <Image position='relative' w='100%' src={Upcommingthree} alt='brand'></Image>
+                            <Image position='relative' w='100%' src={Upcommingfour} alt='brand'></Image>
                                 <Box position='absolute' d='flex' alignItems='center' borderRadius='5px' top='0' background='white' fontSize='sm' m='2' px='3' py='1'>
-                                    
+                                    <svg
+                                        data-swarm-icon="true"
+                                        width="18"
+                                        height="18"
+                                        fill="#757575"
+                                        ml="4"
+                                        d="block"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                        <path d="M11.5 4.5A1.5 1.5 0 0113 6v1.4L16 5v8l-3-2.4V12a1.5 1.5 0 01-1.5 1.5h-8A1.5 1.5 0 012 12V6a1.5 1.5 0 011.5-1.5h8z"></path>
+                                    </svg>
+                                    <Box ml='2' fontWeight='semibold' color='#757575'>
+                                        Online Event
+                                    </Box>
                                 </Box>
                             </Box>
                         </Box>
