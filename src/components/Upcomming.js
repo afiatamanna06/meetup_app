25c2b2d9c26bff04fc7a2f033af5132395652e6d
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-19 23:04:30 +0600

    Update Upcomming.js file

--- src/components/Upcomming.js
+++ src/components/Upcomming.js
@@ -209,6 +209,16 @@ const Upcomming = () => {
                                     <Image borderRadius='50%' border='1px' borderColor='gray.200' boxSize='40px' mr='-8px' src={sageAdebayo} alt="Segun Adebayo"></Image>
                                     <Box ml='16px' color='#757575'>134</Box>
                                 </Box>
+                                <Box>
+                                    <svg
+                                        data-swarm-icon="true"
+                                        height="18"
+                                        width="18"
+                                        viewBox="0 0 18 18"
+                                        >
+                                        <path d="M8 3.415L6.707 4.707a1 1 0 01-1.414-1.414l3.003-3a1 1 0 011.414 0l2.997 3a1 1 0 11-1.414 1.414L10 3.413V9a1 1 0 11-2 0V3.415zM13.5 9a1 1 0 010-2H15a1 1 0 011 1v7.5a1 1 0 01-1 1H3a1 1 0 01-1-.989l-.085-7.5a1 1 0 011-1.011H4.55a1 1 0 110 2h-.624l.063 5.5H14V9h-.5z"></path>
+                                    </svg>
+                                </Box>
                             </Box>
                         </Box>
                     </Grid>
