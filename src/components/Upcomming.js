95cb5fd3ddcc975f5408b59cca9eba08ba0674a8
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-19 20:07:30 +0600

    Update Upcomming.js file

--- src/components/Upcomming.js
+++ src/components/Upcomming.js
@@ -68,7 +68,7 @@ const Upcomming = () => {
                                         height="18"
                                         width="18"
                                         viewBox="0 0 18 18"
-                                    >
+                                        >
                                         <path d="M8 3.415L6.707 4.707a1 1 0 01-1.414-1.414l3.003-3a1 1 0 011.414 0l2.997 3a1 1 0 11-1.414 1.414L10 3.413V9a1 1 0 11-2 0V3.415zM13.5 9a1 1 0 010-2H15a1 1 0 011 1v7.5a1 1 0 01-1 1H3a1 1 0 01-1-.989l-.085-7.5a1 1 0 011-1.011H4.55a1 1 0 110 2h-.624l.063 5.5H14V9h-.5z"></path>
                                     </svg>
                                 </Box>
@@ -120,6 +120,15 @@ const Upcomming = () => {
                                         >
                                         <path d="M8 3.415L6.707 4.707a1 1 0 01-1.414-1.414l3.003-3a1 1 0 011.414 0l2.997 3a1 1 0 11-1.414 1.414L10 3.413V9a1 1 0 11-2 0V3.415zM13.5 9a1 1 0 010-2H15a1 1 0 011 1v7.5a1 1 0 01-1 1H3a1 1 0 01-1-.989l-.085-7.5a1 1 0 011-1.011H4.55a1 1 0 110 2h-.624l.063 5.5H14V9h-.5z"></path>
                                     </svg>
+                                </Box>
+                            </Box>
+                        </Box>
+                        {/* 3rd column */}
+                        <Box width='100%' align='left'>
+                            <Box position='relative'>
+                                <Image position='relative' w='100%' src={Upcommingthree} alt='brand'></Image>
+                                <Box position='absolute' d='flex' alignItems='center' borderRadius='5px' top='0' background='white' fontSize='sm' m='2' px='3' py='1'>
+                                    
                                 </Box>
                             </Box>
                         </Box>
