8579ced6d11ac141120cee77245d0de10aa47c90
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-20 19:06:42 +0600

    Update Connected.js file

--- src/components/Connected.js
+++ src/components/Connected.js
@@ -1,5 +1,10 @@
 import { Box, Container } from '@chakra-ui/react'
 import React from 'react'
+import deviceLeft from '../assets/device-left.webp'
+import deviceRight from '../assets/device-right.webp'
+import meetupLogo from '../assets/meetup-logo.svg'
+import androidAppstore from '../assets/android-app-download.svg'
+import iosAppstore from '../assets/ios-app-download.svg'
 
 const Connected = () => {
     return (
