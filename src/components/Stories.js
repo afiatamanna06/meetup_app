ad59c48e7e79c9743668d48340f970a96d3d2f75
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-20 20:35:16 +0600

    Update Stories.js file

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Storyone from '../assets/story-one.jpg'
 import Storytwo from '../assets/story-two.jpg'
 import Storythree from '../assets/story-three.jpg'
+import { Box, Container } from '@chakra-ui/react'
 
 const Stories = () => {
     return (
         <div>
-            
+            <Box mt='10'>
+                <Container maxW='container.xl'>
+
+                </Container>
+            </Box>
         </div>
     )
 }
