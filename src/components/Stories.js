1abc60bbd645b896704ad338e586e8c12f0942ef
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-20 20:42:06 +0600

    Update Stories.js file

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Storyone from '../assets/story-one.jpg'
 import Storytwo from '../assets/story-two.jpg'
 import Storythree from '../assets/story-three.jpg'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Heading } from '@chakra-ui/react'
 
 const Stories = () => {
     return (
@@ -11,7 +11,9 @@ const Stories = () => {
                 <Container maxW='container.xl'>
                     <Box d='flex' alignItems='center' justifyContent='space-between' mb='7'>
                         <Box align='center' width='100%'>
-
+                            <Heading as='h3' pb='2' alignItems='center' size='lg'>
+                                Stories from Meetups
+                            </Heading>
                         </Box>
                     </Box>
                 </Container>
