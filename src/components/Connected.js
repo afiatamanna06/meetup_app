0aa620a141a9b6ce61a940cf497eaefff681fa85
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-20 19:30:10 +0600

    Update Connected.js file

--- src/components/Connected.js
+++ src/components/Connected.js
@@ -1,5 +1,6 @@
 import { Box, Container } from '@chakra-ui/react'
 import React from 'react'
+import { Image } from '@chakra-ui/image'
 import deviceLeft from '../assets/device-left.webp'
 import deviceRight from '../assets/device-right.webp'
 import meetupLogo from '../assets/meetup-logo.svg'
@@ -12,7 +13,9 @@ const Connected = () => {
             <Box my='20'>
                 <Container maxWidth='container.xl'>
                     <Box pos='relative' justifyContent='space-between' d='flex' alignItems='center'>
-                        <Box></Box>
+                        <Box display='block'>
+                            <Image w='260px' src={deviceLeft}></Image>
+                        </Box>
                     </Box>
                 </Container>
             </Box>
