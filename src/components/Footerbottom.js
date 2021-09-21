a8940729b758e25bf72525a8d0bafe3fc61423e3
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-21 12:39:30 +0600

    Update Footerbottom.js file

--- src/components/Footerbottom.js
+++ src/components/Footerbottom.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import androidAppstore from '../assets/android-app-download.png'
+import iosAppstore from '../assets/ios-app-download.png'
 
 const Footerbottom = () => {
     return (
