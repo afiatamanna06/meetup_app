f759998aef5210fd9178e5023e75a26f6a3dfa6d
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-21 12:42:05 +0600

    Update Footerbottom.js file

--- src/components/Footerbottom.js
+++ src/components/Footerbottom.js
@@ -1,3 +1,4 @@
+import { Box, Container } from '@chakra-ui/layout'
 import React from 'react'
 import androidAppstore from '../assets/android-app-download.png'
 import iosAppstore from '../assets/ios-app-download.png'
@@ -5,7 +6,11 @@ import iosAppstore from '../assets/ios-app-download.png'
 const Footerbottom = () => {
     return (
         <div>
-            
+            <Box color='white' background='#212121' py='10' mt='5'>
+                <Container maxW='container.xl'>
+
+                </Container>
+            </Box>
         </div>
     )
 }
