e92ca28f11f448782fca4368e1d33a3832c45280
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-21 19:09:50 +0600

    Update Footerbottom.js file

--- src/components/Footerbottom.js
+++ src/components/Footerbottom.js
@@ -61,8 +61,19 @@ const Footerbottom = () => {
                     <Box justifyContent='space-between' d='flex' flexDirection='row' alignItems='center' mt='15'>
                         <Box d='flex' flexDirection='column'>
                             <Box mb='2'>Follow us</Box>
-                            <Box>
-                                
+                            <Box d='flex'>
+                                <Link mr='4'>
+                                    <svg
+                                        data-swarm-icon="true"
+                                        height="28"
+                                        fill="#ffffff"
+                                        mr="3"
+                                        width="28"
+                                        viewBox="0 0 28 28"
+                                        >
+                                        <path d="M26.5 14c0-6.904-5.596-12.5-12.5-12.5S1.5 7.096 1.5 14c0 6.24 4.571 11.41 10.547 12.348v-8.735H8.873V14h3.174v-2.754c0-3.133 1.866-4.863 4.721-4.863 1.368 0 2.798.244 2.798.244v3.076H17.99c-1.553 0-2.037.964-2.037 1.952V14h3.467l-.554 3.613h-2.913v8.735C21.93 25.41 26.5 20.24 26.5 14"></path>
+                                    </svg>
+                                </Link>
                             </Box>
                         </Box>
                     </Box>
