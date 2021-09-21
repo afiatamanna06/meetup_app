e0fcf8c9ec9d62810cfc6d7940dc073c1c86c47c
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-21 20:27:00 +0600

    Update app.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ import Main from './components/Main';
 import Popular from './components/Popular';
 import Stories from './components/Stories';
 import Upcomming from './components/Upcomming';
+import imageBg from './assets/bg.svg'
+import { Box } from '@chakra-ui/layout';
 
 function App() {
   return (
     <div className="App">
-      <Banner />
-      <Main />
-      <ItWorks />
-      <Upcomming />
-      <Popular />
-      <Connected />
-      <Stories />
-      <Footerbottom />
+      <Box pos='absolute' left='0' top='0' right='0'>
+        <Banner />
+        <Main />
+        <ItWorks />
+        <Upcomming />
+        <Popular />
+        <Connected />
+        <Stories />
+        <Footerbottom />
+      </Box>
     </div>
   );
 }
