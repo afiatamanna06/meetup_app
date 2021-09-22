af22d6348d40747c9a34749808b3b30754be5ec9
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-22 13:19:36 +0600

    Update styles

--- src/components/ItWorks.js
+++ src/components/ItWorks.js
@@ -12,13 +12,17 @@ const ItWorks = () => {
         <div>
             <Box>
                 <Container maxWidth='container.xl' my='20'>
-                    <Heading textAlign='center' size='lg' pb='2'>How Meetup works</Heading>
+                    <Heading as='h3' textAlign='center' size='lg' pb='2'>How Meetup works</Heading>
                     <Box align='center' mx='auto' maxWidth='700px' pb='10' fontSize='xl'>
                         Meet new people who share your interests through online and in-person
                         events. It’s free to create an account.
                     </Box>
 
-                    <Grid templateColumns='repeat(3, 1fr)' gap='6'>
+                    <Grid templateColumns={{sm: 'repeat(1, 1fr)',
+                                            md: 'repeat(3, 1fr)',
+                                            lg: 'repeat(3, 1fr)',
+                                            xl: 'repeat(3, 1fr)',
+                                            base: 'repeat(1, 1fr)'}} gap='6'>
                         <Box mx='auto' align='center'>
                             <Image w='100%' boxSize='160px' src={joinGroup} alt='brand'></Image>
                             <Heading as='h5' size='md'>
