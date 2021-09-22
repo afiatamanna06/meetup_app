a147633b9b06eb6c4d94cb85d7f1be7a0e6467a4
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-22 19:05:24 +0600

    Update styles

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
             <Box d='flex' alignItems='center' justifyContent='space-between' ml={16} mr={16}>
                 <Image boxSize='90px' src={brandLogo} alt='brand'/>
                 <Box>
-                    <Button _hover={{ color: 'orange.500' }} _focus={{ outline: 0 }} pr={3} mr={3} color='gray'  fontSize='lg' variant='ghost'>Log In</Button>
+                    <Button _hover={{ color: 'blue.100' }} _focus={{ outline: 0 }} pr={3} mr={3} color='gray'  fontSize='lg' variant='ghost'>Log In</Button>
                     <Button _hover={{ color: 'blue.100' }} _focus={{ outline: 0 }} color='gray' fontSize='lg' variant='ghost'>Sign Up</Button>
                 </Box>
             </Box>
