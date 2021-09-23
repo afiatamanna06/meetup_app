8b251dfd00a590257e77a1a38e977dc34ad379b1
afiatamanna06 <afiatamanna06@gmail.com>
2021-09-23 10:17:38 +0600

    Update styles

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,10 +7,10 @@ import { Button } from '@chakra-ui/button'
 const Banner = () => {
     return (
         <div>
-            <Box d='flex' alignItems='center' justifyContent='space-between' ml={16} mr={16}>
+            <Box d='flex' alignItems='center' justifyContent='space-between' ml={10} mr={1}>
                 <Image boxSize='90px' src={brandLogo} alt='brand'/>
-                <Box>
-                    <Button _hover={{ color: 'blue.100' }} _focus={{ outline: 0 }} pr={3} mr={3} color='gray'  fontSize='lg' variant='ghost'>Log In</Button>
+                <Box d='flex' flexDir='row' right='0'>
+                    <Button _hover={{ color: 'blue.100' }} _focus={{ outline: 0 }} pr={1} color='gray'  fontSize='lg' variant='ghost'>Log In</Button>
                     <Button _hover={{ color: 'blue.100' }} _focus={{ outline: 0 }} color='gray' fontSize='lg' variant='ghost'>Sign Up</Button>
                 </Box>
             </Box>
